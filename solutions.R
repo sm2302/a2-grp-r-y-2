57e5021d627c4421cef0ebc4c1981da24b004cb1
Yean19 <19B2068@ubd.edu.bn>
2022-11-02 00:09:01 +0800

    Deleted Random Chord Coordinate
    We deleted the codes for the random chord as we plan to replace it with our own. Kept the equilateral triangle and circle.

--- solutions.R
+++ solutions.R
@@ -15,20 +15,10 @@ eqtri_df <- tibble(
   yend = c(-0.5, -0.5, 1)
 )
 
-# Coordinates of random chord
-rdmchr_df <- tibble(
-  x    = 0.93636368,
-  y    = 0.35103142,
-  xend = -0.9999991,
-  yend = -0.001326758
-)
-
 # Plot
 p <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
   geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
-  geom_segment(data = rdmchr_df, aes(x = x, y = y, xend = xend, yend = yend),
-               col = "red3") +
   coord_equal()
 
 ggsave(p, file = "plot.png", height = 5, width = 7)
