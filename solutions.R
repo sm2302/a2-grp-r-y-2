2bf490ee437302b2363e5b87d60ec2792d8f6f41
Yean19 <19B2068@ubd.edu.bn>
2022-11-02 22:49:40 +0800

    Added codes for Method B
    I tried to code the probability of Method 2 but there are still errors.
    
    Understanding for Method B:
    Chords are drawn perpendicular to the radius and If the chords are closer to the centre than the circumference then the chord is longer than the sides of the equilateral triangle

--- solutions.R
+++ solutions.R
@@ -16,16 +16,59 @@ eqtri_df <- tibble(
 )
 
 # Setting parameters
-xx0 = 0; yy0 = 0; # center of circle
-r = 1; # radius of circle
 nLines = 100; # number of lines
+
+
+
 
 
 # Method A
 
 
+# Plot
+p <- ggplot() +
+  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  coord_equal()
+
+ggsave(p, file = "plot.png", height = 5, width = 7)
+
+
+
+
 
 # Method B
+
+sLines = 0
+lLines = 0
+for (n in nLines){
+  x1 = 0
+  x2 = runif(nLines, min = -1, max = 1)
+  y1 = 0
+  y2 = runif(c(nLines, min = -1, max = 1))
+  b1 <- ((y2-y1)^2) + ((x2-x1)^2)
+  b2 <- sqrt(b1)
+}
+if b2 > sqrt(3) {
+  lLines = lLines + 1
+  }
+  elseif b2 < sqrt(3) {
+    sLines = sLines + 1
+  }
+}
+
+p <- sLines/(sLines + lLines)
+print(p)
+
+# Plot
+p <- ggplot() +
+  ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
+  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  coord_equal()
+
+ggsave(p, file = "plot.png", height = 5, width = 7)
+
+
 
 
 
