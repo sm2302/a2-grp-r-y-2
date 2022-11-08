7512d369470a35a4b88bdbb11245c9a8fb3bf3c1
Yean19 <19B2068@ubd.edu.bn>
2022-11-08 21:12:38 +0800

    Labelled the chords
    Labelled the sum of the chords that is longer than sqrt(3)  and rearrange the code so that probability part is on top and plotting is below

--- solutions.R
+++ solutions.R
@@ -5,6 +5,63 @@ library(tidyverse)
 library(ggforce)
 theme_set(theme_void())
 
+# Bertrand’s Paradox Probability Part
+
+# Setting parameters
+nLines = 100; # Number of lines
+r = 1 # Radius of circle
+l = sqrt(3) # Length of the side of an equilateral triangle
+
+
+
+# Method A
+theta1 <- runif(nLines, 0, 2*pi)
+theta2 <- runif(nLines, 0, 2*pi) 
+# Endpoints of chords within the circle
+x1 <- r*cos(theta1) 
+y1 <- r*sin(theta1) 
+x2 <- r*cos(theta2)
+y2 <- r*sin(theta2)
+
+chordA <- sqrt(((x2)-(x1))^2 + ((y2)-(y1))^2)
+sA <- sum(chordA > l) # Total number of chords longer than the length(l)
+pA <- sA/nLines # Probability of Method A
+
+
+
+# Method B
+theta3 <- runif(nLines, 0, 2*pi)
+# Endpoints of chords within the circle
+x3 <- r*cos(theta3)
+y3 <- r*sin(theta3) 
+
+distance <- runif(nLines, 0, r)
+chordB <- 2 * sqrt((1^2) - (distance)^2)
+sB <- sum(chordB > l) # Total number of chords longer than the length(l)
+pB <- sB/nLines # Probability of Method B
+
+
+
+# Method C 
+theta4 <- runif(nLines, 0, 2*pi)
+# Endpoints of chords within the circle
+x4 <- r*cos(theta4)
+y4 <- r*sin(theta4)
+x5 <- x4*cos(theta4) + y4*sin(theta4)
+y5 <- x4*sin(theta4) - y4*cos(theta4)
+x6 <- x4*cos(theta4) - y4*sin(theta4)
+y6 <- x4*sin(theta4) + y4*cos(theta4)
+
+chordC <- sqrt(((x6)-(x5))^2 + ((y6)-(y5))^2)
+sC <- sum(chordC > l) # Total number of chords longer than the length(l)
+pC <- sC/nLines # Probability of Method C
+
+
+
+
+
+# Bertrand’s Paradox Plotting Part
+
 # Draw a random chord in a unit circle centred at origin -----------------------
 
 # Coordinates of equilateral triangle
@@ -14,50 +71,6 @@ eqtri_df <- tibble(
   xend = c(sqrt(3) / 2, -sqrt(3) / 2, 0),
   yend = c(-0.5, -0.5, 1)
 )
-
-# Setting parameters
-nLines = 1000; # number of lines
-r = 1 # radius of circle
-l = sqrt(3) # length of the side of triangle
-
-
-# Method A
-theta1 <- runif(nLines, 0, 2*pi)
-theta2 <- runif(nLines, 0, 2*pi) 
-# endpoints of chords whithin the circle
-x1 <- r*cos(theta1) 
-y1 <- r*sin(theta1) 
-x2 <- r*cos(theta2)
-y2 <- r*sin(theta2)
-
-chordA <- sqrt(((x2)-(x1))^2 + ((y2)-(y1))^2)
-sum(chordA > l) # total number of chords longer than the length(l)
-pA <- sum(chordA > l) / nLines # probability of Method A
-
-# Method B
-theta3 <- runif(nLines, 0, 2*pi)
-# endpoints of chords within the circle
-x3 <- r*cos(theta3)
-y3 <- r*sin(theta3) 
-
-distance <- runif(nLines, 0, r)
-chordB <- 2 * sqrt((1^2) - (distance)^2)
-sum(chordB > l) # total number of chords longer than the length(l)
-pB <- sum(chordB > l) / nLines # probability of Method B
-
-# Method C 
-theta4 <- runif(nLines, 0, 2*pi)
-# endpoints of chords within the circle
-x4 <- r*cos(theta4)
-y4 <- r*sin(theta4)
-x5 <- x4*cos(theta4) + y4*sin(theta4)
-y5 <- x4*sin(theta4) - y4*cos(theta4)
-x6 <- x4*cos(theta4) - y4*sin(theta4)
-y6 <- x4*sin(theta4) + y4*cos(theta4)
-
-chordC <- sqrt(((x6)-(x5))^2 + ((y6)-(y5))^2)
-sum(chordC > l) # total number of chords longer than the length(l)
-pC <- sum(chordC > l) / nLines # probability of Method C
 
 # Plot
 p <- ggplot() +
