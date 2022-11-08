3e0d0a30783cc2547a7de2dee57ae487e51ce4b3
Yean19 <19B2068@ubd.edu.bn>
2022-11-08 15:49:45 +0800

    Method A
    Finalised the probability part of Method A

--- solutions.R
+++ solutions.R
@@ -16,25 +16,26 @@ eqtri_df <- tibble(
 )
 
 # Setting parameters
-nLines = 1000; # number of lines
+nLines = 10000; # number of lines
+r = 1
 
 
 
 # Method A
-x1 <- runif(nLines,-1,1)
-y1 <- sample(c(-1,1), size = 1) * sqrt(1 - ((x1)^2))
-x2 <- runif(nLines,-1,1)
-y2 <- sample(c(-1,1), size = 1) * sqrt(1 - ((x2)^2))
+theta1 <- runif(nLines, 0, 2*pi)
+theta2 <- runif(nLines, 0, 2*pi) 
+x1 <- r*cos(theta1)
+y1 <- r*sin(theta1) 
+x2 <- r*cos(theta2)
+y2 <- r*sin(theta2)
 chordA <- sqrt(((x2)-(x1))^2 + ((y2)-(y1))^2)
 sum(chordA > sqrt(3))
-
-
 
 
 # Plot
 p <- ggplot() +
   ggforce::geom_circle(aes(x0 = 0, y0 = 0, r = 1), col = "gray50") +
-  geom_segment(data = eqtri_df, aes(x = x, y = y, xend = xend, yend = yend)) +
+  geom_segment(data = eqtri_df, aes(x1 = x1, y1 = y1, x2 = x2, y2 = y2)) +
   coord_equal()
 
 ggsave(p, file = "plot.png", height = 5, width = 7)
